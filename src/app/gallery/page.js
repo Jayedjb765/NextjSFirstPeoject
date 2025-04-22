fd809cbe205711c85f75580561fa913e41c742f4
Jayedjb765 <jayedjb76@gmail.com>
2025-04-22 11:53:07 +0600

    optimize font

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import localimg from '../../assets/authorphotos5-1024x683.jpg'
 
 const GellaryPage = () => {
     return (
@@ -8,6 +9,8 @@ const GellaryPage = () => {
 
             <h1>Regular image tag</h1>
             <Image src="https://i.ibb.co.com/QbmfSKv/images-4.jpg" alt="next image" width={500} height={500} className='mx-auto' />
+            <h1>Local image</h1>
+            <Image src={localimg} alt="next image" width={500} height={500} className='mx-auto' />
         </div>
     );
 };
