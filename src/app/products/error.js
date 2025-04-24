13c6449c1a137aa53614b01dddfb24e91edbc9ac
Jayedjb765 <jayedjb76@gmail.com>
2025-04-24 14:34:31 +0600

    error handeling

--- src/app/products/error.js
+++ src/app/products/error.js
@@ -4,7 +4,13 @@ import React from 'react';
 const ErrorPage = ({ error, reset }) => {
     return (
         <div>
-            <h1 className='text-center text-red-400 my-5 text-xl'>Something went wrong</h1>
+            <div className='text-center text-red-400 my-5 text-xl'>
+                <h1 >Something went wrong</h1>
+                <p>{error.message}</p>
+            </div>
+            <div className="card-actions justify-center">
+                <button onClick={() => reset()} className="btn btn-primary">Try again</button>
+            </div>
         </div>
     );
 };
